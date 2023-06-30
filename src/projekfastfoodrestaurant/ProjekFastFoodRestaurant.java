5677234a2909d8d1a41c3e935f6e4b3236f341a6
Riczky Pratama <furatamarizuki@gmail.com>
2023-06-30 11:04:51 +0700

    fast-food created by Riczky Pratama to commit

---
+++ src/projekfastfoodrestaurant/ProjekFastFoodRestaurant.java
@@ -0,0 +1,16 @@
+package projekfastfoodrestaurant;
+
+/**
+ *
+ * @author furat
+ */
+public class ProjekFastFoodRestaurant {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
